088234c951c376fa8dc633fddeec2d3eac9f1b84
Michelle Tran <mptran0101@berkeley.edu>
2020-08-27 21:38:08 -0700

    switched path to build

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(apiRoutes);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("build"));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "public", "index.html"));
+        res.sendFile(path.resolve(__dirname, "build", "index.html"));
     });
 }
 
